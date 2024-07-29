d6fcd01384299c7f1fd50d996e54299f09805af8
Tej Pratap <bankebihari5463@gmail.com>
2024-07-29 22:56:09 +0530

    Car rental application start

---
+++ CarRental/CarRentalMain.java
@@ -0,0 +1,7 @@
+package CarRental;
+
+public class CarRentalMain {
+    public static void main(String[] args){
+        System.out.println("Car Rental Application started");
+    }
+}
---
+++ factoryPattern/MainClass.java
@@ -0,0 +1,11 @@
+package FactoryPattern;
+
+public class MainClass {
+
+    public static void main(String[] args){
+        ShapeFactory shapeFactoryObj = new ShapeFactory();
+        Shape shapeObj = shapeFactoryObj.getShape("CIRCLE");
+        shapeObj.draw();
+    }
+
+}
