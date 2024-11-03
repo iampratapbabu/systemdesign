c77ea2722fbca06eed8a346ee50d1d11835f94a1
Tej Pratap <bankebihari5463@gmail.com>
2024-11-03 20:14:24 +0530

    new code added

--- WithStrategyPattern/Main.java
+++ WithStrategyPattern/Main.java
@@ -2,7 +2,10 @@ package WithStrategyPattern;
 
 public class Main {
     public static void main(String[] args){
-    Vehicle vc1 = new SportsVehicle();
-    vc1.drive();
+        Vehicle vc1 = new SportsVehicle();
+        vc1.drive();
+
+        Vehicle vc2 = new GoodsVehicle();
+        vc2.drive();
     }
 }
